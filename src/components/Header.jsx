4ac7305586384b80bab8d45015c39da30b7706ca
Vignesh C K <vigneshck2003.code@gmail.com>
2025-09-24 13:32:19 +0530

    changes in the header file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,27 +27,27 @@ export default function Header() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <NavLink className="nav-link" to="/lead">
+                <NavLink className="nav-link">
                   Lead
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/sales">
+                <NavLink className="nav-link">
                   Sales
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/agent">
+                <NavLink className="nav-link">
                   Agent
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/report">
+                <NavLink className="nav-link">
                   Report
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/settings">
+                <NavLink className="nav-link">
                   Settings
                 </NavLink>
               </li>
